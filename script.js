240f92af72970edcd59bb339e4192189b720bda3
Kaofang <benyapa_kh@cmu.ac.th>
2021-07-29 14:13:49 +0700

    author section

--- script.js
+++ script.js
@@ -7,7 +7,15 @@ const color = document.querySelector('#color')
 // define more constants and variables here
 
 btn_toggle.onclick = () => {
-  author.innerHTML = 640631102+length
+  if(btn_toggle.innerHTML === "Show author"){
+    author.innerHTML = "640631102 PANNAWIT PANWONG"
+    btn_toggle.innerHTML = "Show Calculation"
+  }
+  else{
+
+    author.innerHTML = 640631102+ parseInt(length.value)
+  btn_toggle.innerHTML = "Show author"
+  }
 }
 
 // more codes for Search and Reset buttons here
